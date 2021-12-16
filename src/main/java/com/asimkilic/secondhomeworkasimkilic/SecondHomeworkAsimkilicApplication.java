0e89788dc4fa900ab046cdedf21573194ef0272b
asimkilic <a.asim.kilic@gmail.com>
2021-12-16 18:44:41 +0300

    spring added and application.properties configured

---
+++ src/main/java/com/asimkilic/secondhomeworkasimkilic/SecondHomeworkAsimkilicApplication.java
@@ -0,0 +1,13 @@
+package com.asimkilic.secondhomeworkasimkilic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomeworkAsimkilicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHomeworkAsimkilicApplication.class, args);
+	}
+
+}
